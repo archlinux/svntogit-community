9b846958887d33c333ca70d418cabf272963938f
aschaefer <aschaefer@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2009-11-07 15:13:49 +0000

    upgpkg: jgnash 1.12.0-1     Version bump and switch to java-runtime dependency
    git-svn-id: file:///srv/repos/svn-community/svn@5274 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- jgnash/trunk/jgnash.sh
+++ jgnash/trunk/jgnash.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-CP=/usr/share/java/jgnash/jgnash-1.11.7.jar
+CP=/usr/share/java/jgnash/jgnash-1.12.0.jar
 
-exec /opt/java/jre/bin/java -cp $CLASSPATH:$CP -jar /usr/share/java/jgnash/jgnash-1.11.7.jar
+exec $JAVA_HOME/bin/java -cp $CP -jar /usr/share/java/jgnash/jgnash-1.12.0.jar
