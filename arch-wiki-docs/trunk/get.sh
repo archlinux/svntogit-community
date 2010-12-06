2a9ece9561c63289ab12b7d9ef48cc95fc641ecd
spupykin <spupykin@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2010-12-06 09:59:58 +0000

    git-svn-id: file:///srv/repos/svn-community/svn@34314 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- arch-wiki-docs/trunk/get.sh
+++ arch-wiki-docs/trunk/get.sh
@@ -4,7 +4,7 @@ TARGETDIR=$2
 
 mkdir -p $TARGETDIR
 
-cat > $TARGETDIR/index.html <<EOF
+cat >$TARGETDIR/index.html <<EOF
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <HTML>
 	<HEAD>
@@ -18,8 +18,8 @@ $1/index.pl http://wiki.archlinux.org | while read A; do
     TITLE=`echo $A | cut -d \  -f 2- | tr ' ' '_'`
     ID=`echo $A | cut -d \  -f 1`
     echo "$ID => $TITLE"
-    echo "<P><A HREF='$ID.html'>$TITLE</A>" >> $TARGETDIR/index.html
+    echo "<P><A HREF='$ID.html'>$TITLE</A>" >>$TARGETDIR/index.html
     [ -f "$TARGETDIR/$ID.html" ] || wget -q "http://wiki.archlinux.org/index.php?title=$TITLE&printable=yes" -O "$TARGETDIR/$ID.html"
 done
 
-echo "</BODY></HTML>" >> $TARGETDIR/index.html
+echo "</BODY></HTML>" >>$TARGETDIR/index.html
