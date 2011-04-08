a167c9194523c5e0a84917b2a5d5065f9b50eacd
lfleischer <lfleischer@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-04-08 08:10:46 +0000

    archrelease: copy trunk to community-testing-i686
    git-svn-id: file:///srv/repos/svn-community/svn@44536 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ surf/repos/community-testing-i686/config.h
@@ -0,0 +1,40 @@
+/* modifier 0 means no modifier */
+static char *useragent      = "Surf/"VERSION" (X11; U; Unix; en-US) AppleWebKit/531.2+ Compatible (Safari)";
+static char *progress       = "#FF0000";
+static char *progress_trust = "#00FF00";
+static char *stylefile      = ".surf/style.css";
+static char *scriptfile     = ".surf/script.js";
+static char *cookiefile     = ".surf/cookies.txt";
+static time_t sessiontime   = 3600;
+#define NOBACKGROUND 0
+
+#define SETPROP(p, q)     { .v = (char *[]){ "/bin/sh", "-c", \
+	"prop=\"`xprop -id $2 $0 | cut -d '\"' -f 2 | dmenu`\" &&" \
+	"xprop -id $2 -f $1 8s -set $1 \"$prop\"", \
+	p, q, winid, NULL } }
+#define DOWNLOAD(d) { \
+	.v = (char *[]){ "/bin/sh", "-c", \
+	"xterm -e \"wget --load-cookies ~/.surf/cookies.txt '$0';\"", \
+	d, NULL } }
+#define MODKEY GDK_CONTROL_MASK
+static Key keys[] = {
+    /* modifier	            keyval      function    arg             Focus */
+    { MODKEY|GDK_SHIFT_MASK,GDK_r,      reload,     { .b = TRUE } },
+    { MODKEY,               GDK_r,      reload,     { .b = FALSE } },
+    { MODKEY|GDK_SHIFT_MASK,GDK_p,      print,      { 0 } },
+    { MODKEY,               GDK_p,      clipboard,  { .b = TRUE } },
+    { MODKEY,               GDK_y,      clipboard,  { .b = FALSE } },
+    { MODKEY|GDK_SHIFT_MASK,GDK_j,      zoom,       { .i = -1 } },
+    { MODKEY|GDK_SHIFT_MASK,GDK_k,      zoom,       { .i = +1 } },
+    { MODKEY|GDK_SHIFT_MASK,GDK_i,      zoom,       { .i = 0  } },
+    { MODKEY,               GDK_l,      navigate,   { .i = +1 } },
+    { MODKEY,               GDK_h,      navigate,   { .i = -1 } },
+    { MODKEY,               GDK_j,      scroll,     { .i = +1 } },
+    { MODKEY,               GDK_k,      scroll,     { .i = -1 } },
+    { 0,                    GDK_Escape, stop,       { 0 } },
+    { MODKEY,               GDK_o,      source,     { 0 } },
+    { MODKEY,               GDK_g,      spawn,      SETPROP("_SURF_URI", "_SURF_GO") },
+    { MODKEY,               GDK_slash,  spawn,      SETPROP("_SURF_FIND", "_SURF_FIND") },
+    { MODKEY,               GDK_n,      find,       { .b = TRUE } },
+    { MODKEY|GDK_SHIFT_MASK,GDK_n,      find,       { .b = FALSE } },
+};
