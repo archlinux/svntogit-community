b16e3121f2e1462ee72ee871310cca3724a1e000
lfleischer <lfleischer@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-05-10 18:48:28 +0000

    archrelease: copy trunk to community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@46703 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ privoxy/repos/community-x86_64/privoxy.conf.d
@@ -0,0 +1,10 @@
+# Location of the config file.
+PRIVOXY_CONF='/etc/privoxy/config'
+
+# User and group that privoxy will be run as.
+PRIVOXY_USER='privoxy'
+PRIVOXY_GROUP='privoxy'
+
+# Additional arguments.
+PRIVOXY_ARGS="--pidfile /var/run/privoxy.pid"
+
