7dc7705ebdc0a1469a543a3ab8b5b0f1431dcb5d
spupykin <spupykin@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-07-22 19:54:48 +0000

    archrelease: copy trunk to community-any
    git-svn-id: file:///srv/repos/svn-community/svn@52269 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ arch-wiki-docs/repos/community-any/index.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+use Encode;
+use JSON::XS;
+
+$URL=$ARGV[0];
+
+@ALLPAGES=();
+
+my $pageid;
+my $pagetitle;
+my $from = "";
+my $ret;
+again:
+my $count = 0;
+$cmd="wget --no-check-certificate -q \"$URL/api.php?action=query&list=allpages&aplimit=500&format=json&apfilterredir=nonredirects&apfrom=$from\" -O -";
+#print STDERR $cmd."\n";
+$text=`$cmd`;
+$ret = JSON::XS->new->utf8->decode($text);
+$H = $ret->{query}->{allpages};
+foreach $i (@$H)
+{
+    push @ALLPAGES, encode("UTF-8", "$i->{title}");
+    printf("%08u %s", $i->{pageid}, encode("UTF-8", "$i->{title}\n"));
+    $count++;
+}
+
+if($count == 1)
+{
+    exit 0;
+}
+
+@ALLPAGES = sort @ALLPAGES;
+$from = $ALLPAGES[-1];
+goto again;
