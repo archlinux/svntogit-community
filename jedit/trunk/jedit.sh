a50478596cfd62fe12193683a3ac45a7e65938af
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-10-27 11:03:16 +0000

    Removed the concept of a default JAVA_HOME as it's not needed
    git-svn-id: file:///srv/repos/svn-community/svn@57417 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- jedit/trunk/jedit.sh
+++ jedit/trunk/jedit.sh
@@ -1,24 +1,16 @@
 #!/bin/sh
-#
-# Runs jEdit - Programmer's Text Editor.
-#
+# Runs jEdit - Programmer's Text Editor
 
-# Set jvm heap initial and maximum sizes (in megabytes).
-JAVA_HEAP_MAX_SIZE=192
-
-DEFAULT_JAVA_HOME="/usr/lib/jvm/java-6-openjdk"
-if [ -z "$JAVA_HOME" ]; then
-  JAVA_HOME="$DEFAULT_JAVA_HOME"
-fi
-
-# If $HOME/.jedit/properties exists, don't do anything.
-# If not, copy in a default file
+# If $HOME/.jedit or $HOME/.jedit/properties exists, don't do anything.
+# If not, copy in a default property file.
 if [ ! -e $HOME/.jedit ]; then
-  mkdir $HOME/.jedit
   if [ ! -e $HOME/.jedit/properties ]; then
+    mkdir $HOME/.jedit
 		cp /usr/share/jedit/default.props $HOME/.jedit/properties
   fi
 fi
 
-# Launch application
-exec "$JAVA_HOME/jre/bin/java" -Xmx${JAVA_HEAP_MAX_SIZE}M -jar "/usr/share/java/jedit/jedit.jar" "$@"
+# Set jvm heap initial and maximum sizes (in megabytes).
+JAVA_HEAP_MAX_SIZE=192
+
+exec java -Xmx${JAVA_HEAP_MAX_SIZE}M -jar "/usr/share/java/jedit/jedit.jar" "$@"
