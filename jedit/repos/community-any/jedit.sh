a59113316bfe64333cea690687b2ce4b1449710c
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-10-27 11:04:47 +0000

    archrelease: copy trunk to community-any
    git-svn-id: file:///srv/repos/svn-community/svn@57418 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ jedit/repos/community-any/jedit.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Runs jEdit - Programmer's Text Editor
+
+# If $HOME/.jedit or $HOME/.jedit/properties exists, don't do anything.
+# If not, copy in a default property file.
+if [ ! -e $HOME/.jedit ]; then
+  if [ ! -e $HOME/.jedit/properties ]; then
+    mkdir $HOME/.jedit
+		cp /usr/share/jedit/default.props $HOME/.jedit/properties
+  fi
+fi
+
+# Set jvm heap initial and maximum sizes (in megabytes).
+JAVA_HEAP_MAX_SIZE=192
+
+exec java -Xmx${JAVA_HEAP_MAX_SIZE}M -jar "/usr/share/java/jedit/jedit.jar" "$@"
