29716e639ffff53b5061dd7d6070e937538514c1
daenyth <daenyth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-12-05 02:15:36 +0000

    archrelease: copy trunk to community-i686, community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@60089 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ tremulous/repos/community-x86_64/tremulous.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+CARCH=`uname -m`
+ext=x86; [ "$CARCH" = 'x86_64' ] && ext=x86_64
+
+if [ ! -r "$HOME/.tremulous/base/vm/game.qvm" ]; then
+	echo "Cannot find game.qvm -- linking to installed version."
+	if [ ! -d "$HOME/.tremulous/base/vm" ]; then
+		mkdir -p "$HOME/.tremulous/base/vm/" || exit 1
+	fi
+	ln -s /opt/tremulous/game.qvm "$HOME/.tremulous/base/vm/game.qvm" || exit 1
+fi
+
+cd /opt/tremulous/
+./tremulous.$ext "$@"
