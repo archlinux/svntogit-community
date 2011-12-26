de01c1f573dc07f0f26a8d330e6e3a8ccb1cfc4e
dreisner <dreisner@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2011-12-26 17:36:12 +0000

    archrelease: copy trunk to community-i686, community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@61258 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-i686/stunnel.rc.d
+++ repos/community-i686/stunnel.rc.d
@@ -3,26 +3,35 @@
 . /etc/rc.conf
 . /etc/rc.d/functions
 
-PID=`pidof -o %PPID /usr/bin/stunnel`
+pidfile=/var/run/stunnel.pid
+if [[ -r $pidfile ]]; then
+  read -r PID < "$pidfile"
+  if [[ $PID && ! -d /proc/$PID ]]; then
+    # stale pidfile
+    unset PID
+    rm -f "$pidfile"
+  fi
+fi
+
 case "$1" in
   start)
     stat_busy "Starting stunnel"
-    [ -z "$PID" ] && /usr/bin/stunnel
-    if [ $? -gt 0 ]; then
-      stat_fail
-    else
+    if [[ -z $PID ]] && /usr/bin/stunnel; then
       add_daemon stunnel
       stat_done
+    else
+      stat_fail
+      exit 1
     fi
     ;;
   stop)
     stat_busy "Stopping stunnel"
-    [ ! -z "$PID" ]  && kill $PID &> /dev/null
-    if [ $? -gt 0 ]; then
-      stat_fail
-    else
+    if [[ $PID ]] && kill $PID &>/dev/null; then
       rm_daemon stunnel
       stat_done
+    else
+      stat_fail
+      exit 1
     fi
     ;;
   restart)
@@ -31,6 +40,5 @@ case "$1" in
     $0 start
     ;;
   *)
-    echo "usage: $0 {start|stop|restart}"  
+    echo "usage: $0 {start|stop|restart}"
 esac
-exit 0
