71816a5dbd313cf17312b3fc133c773247ef1c7e
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-03-18 13:46:05 +0000

    Fixed FS#28953
    git-svn-id: file:///srv/repos/svn-community/svn@68114 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- unetbootin/trunk/unetbootin.sh
+++ unetbootin/trunk/unetbootin.sh
@@ -1,2 +1,25 @@
 #!/bin/sh
-gksudo /usr/bin/unetbootin.elf
+# sudo/su wrapper
+ELF=/usr/bin/unetbootin.elf
+if [ -z "$KDE_FULL_SESSION" ]; then
+  if [ -e /usr/bin/gksudo ]; then
+      gksudo $ELF
+      exit 0
+  fi
+  if [ -e /usr/bin/gksu ]; then
+      gksu $ELF
+      exit 0
+  fi
+  echo "Could not use gksudo or gksu"
+  exit 1
+fi  
+if [ -e /usr/bin/kdesudo ]; then
+  kdesudo $ELF
+  exit 0
+fi
+if [ -e /usr/bin/kdesu ]; then
+  kdesu $ELF
+  exit 0
+fi
+echo "Could not use kdesudo or kdesu"
+exit 1
