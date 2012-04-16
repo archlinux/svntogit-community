1c48446042e918fffe031f10ad878e96f22aa4cf
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-04-16 18:18:52 +0000

    archrelease: copy trunk to community-x86_64, community-i686
    git-svn-id: file:///srv/repos/svn-community/svn@69479 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- unetbootin/repos/community-x86_64/unetbootin.sh
+++ unetbootin/repos/community-x86_64/unetbootin.sh
@@ -3,23 +3,19 @@
 ELF=/usr/bin/unetbootin.elf
 if [ -z "$KDE_FULL_SESSION" ]; then
   if [ -e /usr/bin/gksudo ]; then
-      gksudo $ELF
-      exit 0
+      gksudo $ELF && exit 0
   fi
   if [ -e /usr/bin/gksu ]; then
-      gksu $ELF
-      exit 0
+      gksu $ELF && exit 0
   fi
   echo "Could not use gksudo or gksu"
-  exit 1
+  $ELF && exit 0 || exit 1
 fi  
 if [ -e /usr/bin/kdesudo ]; then
-  kdesudo $ELF
-  exit 0
+  kdesudo $ELF && exit 0
 fi
 if [ -e /usr/bin/kdesu ]; then
-  kdesu $ELF
-  exit 0
+  kdesu $ELF && exit 0
 fi
 echo "Could not use kdesudo or kdesu"
-exit 1
+$ELF && exit 0 || exit 1
