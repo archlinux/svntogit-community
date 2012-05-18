1beb4877f4262b8edc69629bf58755e4851e416b
seblu <seblu@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-05-18 22:22:12 +0000

    clean handler.sh
    git-svn-id: file:///srv/repos/svn-community/svn@70898 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- acpid/trunk/handler.sh
+++ acpid/trunk/handler.sh
@@ -9,16 +9,24 @@ set $*
 
 case "$1" in
     button/power)
-        #echo "PowerButton pressed!">/dev/tty5
         case "$2" in
-            PBTN|PWRF)  logger "PowerButton pressed: $2" ;;
-            *)          logger "ACPI action undefined: $2" ;;
+            PBTN|PWRF)
+                logger "PowerButton pressed: $2"
+                poweroff
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
         esac
         ;;
     button/sleep)
         case "$2" in
-            SLPB|SBTN)   echo -n mem >/sys/power/state ;;
-            *)      logger "ACPI action undefined: $2" ;;
+            SLPB|SBTN)
+                echo -n mem >/sys/power/state
+                ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
         esac
         ;;
     ac_adapter)
@@ -28,43 +36,51 @@ case "$1" in
                     00000000)
                         echo -n $minspeed >$setspeed
                         #/etc/laptop-mode/laptop-mode start
-                    ;;
+                        ;;
                     00000001)
                         echo -n $maxspeed >$setspeed
                         #/etc/laptop-mode/laptop-mode stop
-                    ;;
+                        ;;
                 esac
                 ;;
-            *)  logger "ACPI action undefined: $2" ;;
+            *)
+                logger "ACPI action undefined: $2"
+                ;;
         esac
         ;;
     battery)
         case "$2" in
             BAT0)
                 case "$4" in
-                    00000000)   #echo "offline" >/dev/tty5
-                    ;;
-                    00000001)   #echo "online"  >/dev/tty5
-                    ;;
+                    00000000)
+                        logger 'Battery online'
+                        ;;
+                    00000001)
+                        logger 'Battery offline'
+                        ;;
                 esac
                 ;;
-            CPU0)	
+            CPU0)
                 ;;
             *)  logger "ACPI action undefined: $2" ;;
         esac
         ;;
     button/lid)
-       	case "$3" in
-	    close)
-		#echo "LID closed!">/dev/tty5
-		;;
-	    open)
-		#echo "LID opened!">/dev/tty5
-        	;;
-	esac
-	;;
-
+        case "$3" in
+            close)
+                logger 'LID closed'
+                ;;
+            open)
+                logger 'LID opened'
+                ;;
+            *)
+                logger "ACPI action undefined: $3"
+                ;;
+    esac
+    ;;
     *)
         logger "ACPI group/action undefined: $1 / $2"
         ;;
 esac
+
+# vim:set ts=4 sw=4 ft=sh et:
