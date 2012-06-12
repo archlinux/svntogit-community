930980c0900f515524b3e38dd10fbed92f3589b8
ttopper <ttopper@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-06-12 20:24:14 +0000

    upgpkg: polipo 1.0.4.1-3
    git-svn-id: file:///srv/repos/svn-community/svn@72348 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ polipo/trunk/polipo.conf.d
@@ -0,0 +1,4 @@
+#
+# Parameters to be passed to polipo
+#
+POLIPO_ARGS="daemonise=true logFile=\"/var/log/polipo.log\""
--- polipo/trunk/polipo.rc.d
+++ polipo/trunk/polipo.rc.d
@@ -1,60 +1,71 @@
 #!/bin/bash
+
+daemon_name=polipo
+
 . /etc/rc.conf
 . /etc/rc.d/functions
+. /etc/conf.d/$daemon_name.conf
 
-DAEMON=polipo
-ARGS="daemonise=true pidFile=/var/run/$DAEMON/$DAEMON.pid"
-PID=$(pidof -o %PPID /usr/bin/$DAEMON)
+get_pid() {
+	pidof -o %PPID $daemon_name
+}
 
-case $1 in
-    start)
-        stat_busy "Starting $DAEMON"
-        if ck_daemon $DAEMON; then
-            [[ ! -d /var/run/$DAEMON ]] && install -d $DAEMON /var/run/$DAEMON
-            /usr/bin/$DAEMON $ARGS >/dev/null 2>&1
-            if [[ $? != 0 ]]; then
-                stat_fail
-            else
-                add_daemon polipo
-                stat_done
-            fi
-        else
-            stat_fail
-        fi
-    ;;
-    stop)
-        stat_busy "Stopping $DAEMON"
-        if ! ck_daemon $DAEMON; then
-            kill $PID >/dev/null 2>&1
-            if [[ $? != 0 ]]; then
-                stat_fail
-            else
-                rm_daemon $DAEMON
-                stat_done
-            fi
-        else
-            stat_fail
-        fi
-    ;;
-    purge)
-        stat_busy "Purging $DAEMON"
-        [[ ! -d /var/run/$DAEMON ]] && install -d $DAEMON /var/run/$DAEMON
-        if ! ck_daemon $DAEMON; then
-            kill -USR1 $PID >/dev/null 2>&1
-            sleep 1
-            /usr/bin/$DAEMON -x $ARGS >/dev/null 2>&1 || stat_fail
-            kill -USR2 $PID >/dev/null 2>&1
-            stat_done
-        else
-            /usr/bin/$DAEMON -x $ARGS >/dev/null 2>&1 || stat_fail
-            stat_done
-        fi
-    ;;
-    restart)
-        $0 stop
-        $0 start
-    ;;
-    *)
-        echo "usage: $0 {start|stop|restart|purge}"
-    ;;
+case "$1" in
+	start)
+		stat_busy "Starting $daemon_name daemon"
+
+		PID=$(get_pid)
+		if [[ -z $PID ]]; then
+			[[ -f /run/$daemon_name.pid ]] &&
+				rm -f /run/$daemon_name.pid
+		# RUN
+		sudo -u nobody /usr/bin/$daemon_name ${POLIPO_ARGS}
+		#
+		if [[ $? -gt 0 ]]; then
+			stat_fail
+			exit 1
+		else
+			echo $(get_pid) > /run/$daemon_name.pid
+			add_daemon $daemon_name
+			stat_done
+		fi
+		else
+			stat_fail
+			exit 1
+		fi
+		;;
+
+	stop)
+		stat_busy "Stopping $daemon_name daemon"
+		PID=$(get_pid)
+		# KILL
+		[[ -n $PID ]] && kill $PID &> /dev/null
+		#
+		if [[ $? -gt 0 ]]; then
+			stat_fail
+			exit 1
+		else
+			rm -f /run/$daemon_name.pid &> /dev/null
+			rm_daemon $daemon_name
+			stat_done
+		fi
+		;;
+
+	restart)
+		$0 stop
+		sleep 3
+		$0 start
+		;;
+
+	status)
+		stat_busy "Checking $daemon_name status";
+		ck_status $daemon_name
+		;;
+
+	*)
+		echo "usage: $0 {start|stop|restart|status}"
 esac
+
+exit 0
+
+# vim:set ts=2 sw=2 et:
