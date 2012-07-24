f17d0861b601b20b2e7676ed9edfc0efa4b16b84
spupykin <spupykin@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-07-24 15:02:00 +0000

    git-svn-id: file:///srv/repos/svn-community/svn@74311 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/open-vm-tools.conf.d
+++ trunk/open-vm-tools.conf.d
@@ -1,6 +1,9 @@
 # Enable support for Drag'n'Drop
 VM_DRAG_AND_DROP="yes"
 
+# Load these modules on startup
+VM_MODULES=('vmci' 'vsock' 'vmhgfs' 'vmsync')
+
 # vmware-guestd settings
 GUESTD_BIN="/usr/sbin/vmware-guestd"
 PIDFILE="/var/run/vmware-guestd.pid"
