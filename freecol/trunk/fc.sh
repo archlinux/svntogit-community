688866da0f38b01b5e86a04059ef44063de68f6c
svenstaro <svenstaro@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-08-12 15:27:03 +0000

    upgpkg: freecol 0.10.5-2
    Removing forced render stuff from launcher
    
    git-svn-id: file:///srv/repos/svn-community/svn@75044 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- freecol/trunk/fc.sh
+++ freecol/trunk/fc.sh
@@ -5,5 +5,5 @@ LOG_PATH=/tmp/freecol
 
 [ ! -d "$LOG_PATH" ] && install -dm755 $LOG_PATH
 cd $LOG_PATH
-java -Xmx512M -Dsun.java2d.pmoffscreen=false -jar $FC_PATH/FreeCol.jar "$@" --freecol-data $FC_PATH/data \
+java -Xmx512M -jar $FC_PATH/FreeCol.jar "$@" --freecol-data $FC_PATH/data \
   &> ./messages.log
