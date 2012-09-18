e2790a8084f61bb70d2a5590a7e92a20532b1156
seblu <seblu@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-09-18 19:13:44 +0000

    simplify build
    git-svn-id: file:///srv/repos/svn-community/svn@76473 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- virtualbox-modules/trunk/build.sh
+++ virtualbox-modules/trunk/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if (( $# != 2 )); then
-  echo "usage: $0 extra multilib"
-  echo "       $0 testing multilib-testing"
+if (( $# != 1 )); then
+  echo "usage: $0 extra"
+  echo "       $0 testing"
   exit 1
 fi
 
@@ -11,10 +11,10 @@ linux32 makechrootpkg -I ../../virtualbox/trunk/virtualbox-host-source-*-i686.pk
 linux32 makechrootpkg -I ../../virtualbox/trunk/virtualbox-guest-source-*-i686.pkg.tar.xz  -r /var/lib/archbuild/$1-i686
 linux32 makechrootpkg -r /var/lib/archbuild/$1-i686
 
-$2-build
-makechrootpkg -I ../../virtualbox/trunk/virtualbox-host-source-*-x86_64.pkg.tar.xz -r /var/lib/archbuild/$2-x86_64
-makechrootpkg -I ../../virtualbox/trunk/virtualbox-guest-source-*-x86_64.pkg.tar.xz  -r /var/lib/archbuild/$2-x86_64
-makechrootpkg -r /var/lib/archbuild/$2-x86_64
+$1-x86_64-build
+makechrootpkg -I ../../virtualbox/trunk/virtualbox-host-source-*-x86_64.pkg.tar.xz -r /var/lib/archbuild/$1-x86_64
+makechrootpkg -I ../../virtualbox/trunk/virtualbox-guest-source-*-x86_64.pkg.tar.xz  -r /var/lib/archbuild/$1-x86_64
+makechrootpkg -r /var/lib/archbuild/$1-x86_64
 
 
 # vim:set ts=2 sw=2 ft=sh et:
