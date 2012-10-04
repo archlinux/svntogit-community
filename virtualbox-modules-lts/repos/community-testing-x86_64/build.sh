f2f0d150ccd236a3baa5a40039bf39195fec3e60
bpiotrowski <bpiotrowski@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2012-10-04 16:56:39 +0000

    archrelease: copy trunk to community-testing-i686, community-testing-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@77089 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ virtualbox-modules-lts/repos/community-testing-x86_64/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if (( $# != 1 )); then
+  echo "usage: $0 extra"
+  echo "       $0 testing"
+  exit 1
+fi
+
+$1-i686-build
+linux32 makechrootpkg -I ../../virtualbox/trunk/virtualbox-host-source-*-i686.pkg.tar.xz -r /var/lib/archbuild/$1-i686
+linux32 makechrootpkg -I ../../virtualbox/trunk/virtualbox-guest-source-*-i686.pkg.tar.xz  -r /var/lib/archbuild/$1-i686
+linux32 makechrootpkg -r /var/lib/archbuild/$1-i686
+
+$1-x86_64-build
+makechrootpkg -I ../../virtualbox/trunk/virtualbox-host-source-*-x86_64.pkg.tar.xz -r /var/lib/archbuild/$1-x86_64
+makechrootpkg -I ../../virtualbox/trunk/virtualbox-guest-source-*-x86_64.pkg.tar.xz  -r /var/lib/archbuild/$1-x86_64
+makechrootpkg -r /var/lib/archbuild/$1-x86_64
+
+
+# vim:set ts=2 sw=2 ft=sh et:
