cd42a877abfd912a56011ed77c254f47488679a5
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2013-01-04 16:35:56 +0000

    upgpkg: unetbootin 583-1
    git-svn-id: file:///srv/repos/svn-community/svn@81952 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- unetbootin/trunk/unetbootin.sh
+++ unetbootin/trunk/unetbootin.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-pkexec /usr/bin/unetbootin.elf
+# wrapper script for starting unetbootin with root permissions
+ELF=/usr/bin/unetbootin.elf
+if [ -e /usr/bin/pkexec ]; then
+  pkexec $ELF && exit 0
+fi
+if [ -z "$KDE_FULL_SESSION" ]; then
+  if [ -e /usr/bin/gksudo ]; then
+      gksudo $ELF && exit 0
+  fi
+  if [ -e /usr/bin/gksu ]; then
+      gksu $ELF && exit 0
+  fi
+  echo 'Could not use pkexec, gksudo or gksu'
+  $ELF && exit 0 || exit 1
+fi  
+if [ -e /usr/bin/kdesudo ]; then
+  kdesudo $ELF && exit 0
+fi
+if [ -e /usr/bin/kdesu ]; then
+  kdesu $ELF && exit 0
+fi
+echo 'Could not use kdesudo or kdesu'
+$ELF
