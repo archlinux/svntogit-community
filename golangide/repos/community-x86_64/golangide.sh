4f5019a45bb9b121051f5ac55a11e4ecbe332c51
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2013-01-16 23:36:09 +0000

    archrelease: copy trunk to community-x86_64, community-i686
    git-svn-id: file:///srv/repos/svn-community/svn@82408 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- golangide/repos/community-x86_64/golangide.sh
+++ golangide/repos/community-x86_64/golangide.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+ELF=/usr/bin/liteide
 # The libqjson issue has been reported upstream
 if [ ! -z $GOROOT -a -e $GOROOT ]; then
-  LD_PRELOAD=/usr/lib/libqjson.so /usr/bin/liteidex $@
+  LD_PRELOAD=/usr/lib/libqjson.so $ELF $@
 else
-  GOROOT=/usr/lib/go LD_PRELOAD=/usr/lib/libqjson.so /usr/bin/liteidex $@
+  GOROOT=/usr/lib/go LD_PRELOAD=/usr/lib/libqjson.so $ELF $@
 fi
