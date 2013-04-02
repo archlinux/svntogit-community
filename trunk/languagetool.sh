9ca4dad9e682a6c30ecd3ed3351cec6443181ebc
aginiewicz <aginiewicz@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2013-04-02 18:01:39 +0000

    upgpkg: languagetool 2.1-1
    update to languagetool 2.1, added server to script
    
    git-svn-id: file:///srv/repos/svn-community/svn@87534 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/languagetool.sh
+++ trunk/languagetool.sh
@@ -2,10 +2,14 @@
 
 file_present=false
 asked_for_help=false
+start_server=false
 
 for arg in "$@"; do
   if [[ $arg != -* ]]; then
     file_present=true
+  fi
+  if [[ $arg == --config* ]]; then
+    start_server=true
   fi
   if [[ $arg == -h ]]; then
     asked_for_help=true
@@ -19,8 +23,10 @@ CP=/usr/share/languagetool
 for name in /usr/share/java/languagetool/*.jar ; do
   CP=$CP:$name
 done
-CLI_command='org.languagetool.Main'
+
+CLI_command='org.languagetool.commandline.Main'
 GUI_command='org.languagetool.gui.Main'
+SRV_command='org.languagetool.server.HTTPSServer'
 
 if $asked_for_help; then
   echo "Command-line interface (CLI) help:"
@@ -28,11 +34,18 @@ if $asked_for_help; then
   echo
   echo "Graphical user interface (GUI) help:"
   "$JAVA_HOME/bin/java" -cp $CP $GUI_command -h | sed "s/java org.languagetool.gui.Main/languagetool/"
+  echo
+  echo "HTTPS server help:"
+  "$JAVA_HOME/bin/java" -cp $CP $SRV_command -h | sed "s/HTTPSServer/languagetool/"
 else
-  if $file_present; then
-    "$JAVA_HOME/bin/java" -cp $CP $CLI_command "$@"
+  if $start_server; then
+    "$JAVA_HOME/bin/java" -cp $CP $SRV_command "$@"
   else
-    "$JAVA_HOME/bin/java" -cp $CP $GUI_command "$@"
+    if $file_present; then
+      "$JAVA_HOME/bin/java" -cp $CP $CLI_command "$@"
+    else
+      "$JAVA_HOME/bin/java" -cp $CP $GUI_command "$@"
+    fi
   fi
 fi
 
