0ae4be7ec9de932b60d99ae53e165243a47c6026
seblu <seblu@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2013-09-07 00:03:14 +0000

    archrelease: copy trunk to community-i686, community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@96875 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- virtualbox-modules-lts/repos/community-x86_64/build.sh
+++ virtualbox-modules-lts/repos/community-x86_64/build.sh
@@ -1,25 +1,60 @@
 #!/bin/bash
+# lazyness can be enhanced everyday
 
-set -e
-
-if (( $# != 1 )); then
+usage() {
   echo "usage: $0 extra"
   echo "       $0 testing"
   exit 1
-fi
+}
 
-here=$PWD
-cd /var/empty
-linux32 makechrootpkg -cu -I "$here"/../../virtualbox/trunk/virtualbox-host-dkms-*-i686.pkg.tar.xz -r /var/lib/archbuild/$1-i686
-linux32 makechrootpkg -I "$here"/../../virtualbox/trunk/virtualbox-guest-dkms-*-i686.pkg.tar.xz  -r /var/lib/archbuild/$1-i686
-cd "$here"
-linux32 makechrootpkg -n -r /var/lib/archbuild/$1-i686
+# $1: reference package
+update() {
+  url="https://www.archlinux.org/packages/$1/x86_64/$2/"
+  curkernel=$(wget -qO- "$url"|sed -nr "s/.*<h2>$2 ([0-9]+)\.([0-9]+).*<\/h2>.*/\1.\2/p")
+  nextkernel=${curkernel%.*}.$(( ${curkernel#*.}+1))
 
-cd /var/empty
-makechrootpkg -cu -I "$here"/../../virtualbox/trunk/virtualbox-host-dkms-*-x86_64.pkg.tar.xz -r /var/lib/archbuild/$1-x86_64
-makechrootpkg -I "$here"/../../virtualbox/trunk/virtualbox-guest-dkms-*-x86_64.pkg.tar.xz  -r /var/lib/archbuild/$1-x86_64
-cd "$here"
-makechrootpkg -n -r /var/lib/archbuild/$1-x86_64
+  echo "** Current kernel: $curkernel"
+  echo "** Next kernel: $nextkernel"
 
+  sed -ri \
+    -e "s/(_?extramodules=).*-(ARCH|lts).*/\1extramodules-$curkernel-\2/i" \
+    -e "s/(linux.*>=)[0-9]+.[0-9]+/\1$curkernel/" \
+    -e "s/(linux.*<)[0-9]+.[0-9]+/\1$nextkernel/" \
+    PKGBUILD *.install
+}
+
+# $1: repo
+# $2: arch
+build() {
+  _pwd=$PWD
+  cd /var/empty
+  makechrootpkg -cu -I "$_pwd"/../../virtualbox/trunk/virtualbox-host-dkms-*-$arch.pkg.tar.xz -r "$1"
+  makechrootpkg -I "$_pwd"/../../virtualbox/trunk/virtualbox-guest-dkms-*-$arch.pkg.tar.xz  -r "$1"
+  cd "$_pwd"
+  makechrootpkg -n -r "$1"
+}
+
+(( $# == 1 )) || usage
+
+# detect lts case
+grep -q linux-lts PKGBUILD && suf=-lts
+
+case $1 in
+  extra)
+    update core linux$suf-headers
+    for arch in i686 x86_64; do
+      build /var/lib/archbuild/extra-$arch $arch
+    done
+  ;;
+  testing)
+    update testing linux$suf-headers
+    for arch in i686 x86_64; do
+      build /var/lib/archbuild/testing-$arch $arch
+    done
+  ;;
+  *)
+    usage
+  ;;
+esac
 
 # vim:set ts=2 sw=2 ft=sh et:
