1ba94e569be337bf5d086396764ea9c74deec6d2
fyan <fyan@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2013-10-16 14:06:09 +0000

    archrelease: copy trunk to community-testing-i686, community-testing-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@98656 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ cgminer/repos/community-testing-x86_64/cgminer.conf.d
@@ -0,0 +1,11 @@
+# Use JSON-format configuration. (recommended)
+CGMINER_CONF="/etc/cgminer.conf"
+
+# Uncommend this if you prefer simple command-line arguments.
+#CGMINER_CONF=""
+#CGMINER_PROTOCOL="http"
+#CGMINER_IP="127.0.0.1"
+#CGMINER_PORT="8332"
+#CGMINER_USER="username"
+#CGMINER_PASS="password"
+CGMINER_ARGS="--syslog"
