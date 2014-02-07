e54a1d6a246e53462e5bfe2f1a7a6a02ad057b5f
aginiewicz <aginiewicz@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2014-02-07 12:35:53 +0000

    archrelease: copy trunk to community-any
    git-svn-id: file:///srv/repos/svn-community/svn@105427 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- languagetool/repos/community-any/languagetool.sh
+++ languagetool/repos/community-any/languagetool.sh
@@ -3,20 +3,32 @@
 file_present=false
 asked_for_help=false
 start_server=false
+https_server=false
 
+declare -a argv=("$@")
+
+i=0
 for arg in "$@"; do
   if [[ $arg != -* ]]; then
     file_present=true
   fi
   if [[ $arg == --config* ]]; then
     start_server=true
+    https_server=true
+  fi
+  if [[ $arg == --http ]]; then
+    start_server=true
+    unset argv[$i]
   fi
   if [[ $arg == -h ]]; then
     asked_for_help=true
+    unset argv[$i]
   fi
   if [[ $arg == --help ]]; then
     asked_for_help=true
+    unset argv[$i]
   fi
+  ((i++))
 done
 
 CP=/usr/share/languagetool
@@ -26,25 +38,33 @@ done
 
 CLI_command='org.languagetool.commandline.Main'
 GUI_command='org.languagetool.gui.Main'
-SRV_command='org.languagetool.server.HTTPSServer'
+NSRV_command='org.languagetool.server.HTTPServer'
+SSRV_command='org.languagetool.server.HTTPSServer'
 
 if $asked_for_help; then
   echo "Command-line interface (CLI) help:"
-  "$JAVA_HOME/bin/java" -cp $CP $CLI_command -h | sed "s/java -jar LanguageTool.jar/languagetool/"
+  "$JAVA_HOME/bin/java" -cp $CP $CLI_command -h | sed "s/java -jar languagetool-commandline.jar/languagetool/"
   echo
   echo "Graphical user interface (GUI) help:"
   "$JAVA_HOME/bin/java" -cp $CP $GUI_command -h | sed "s/java org.languagetool.gui.Main/languagetool/"
   echo
+  echo "HTTP server help:"
+  "$JAVA_HOME/bin/java" -cp $CP $NSRV_command -h | sed "s/HTTPServer/languagetool --http/"
+  echo
   echo "HTTPS server help:"
-  "$JAVA_HOME/bin/java" -cp $CP $SRV_command -h | sed "s/HTTPSServer/languagetool/"
+  "$JAVA_HOME/bin/java" -cp $CP $SSRV_command -h | sed "s/HTTPSServer/languagetool/"
 else
   if $start_server; then
-    "$JAVA_HOME/bin/java" -cp $CP $SRV_command "$@"
+    if $https_server; then
+      "$JAVA_HOME/bin/java" -cp $CP $SSRV_command "${argv[@]}"
+    else
+      "$JAVA_HOME/bin/java" -cp $CP $NSRV_command "${argv[@]}"
+    fi
   else
     if $file_present; then
-      "$JAVA_HOME/bin/java" -cp $CP $CLI_command "$@"
+      "$JAVA_HOME/bin/java" -cp $CP $CLI_command "${argv[@]}"
     else
-      "$JAVA_HOME/bin/java" -cp $CP $GUI_command "$@"
+      "$JAVA_HOME/bin/java" -cp $CP $GUI_command "${argv[@]}"
     fi
   fi
 fi
