7a96af42e8ad553e4ce40e317205171cca3e3855
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2014-02-09 11:01:22 +0000

    archrelease: copy trunk to community-x86_64, community-i686
    git-svn-id: file:///srv/repos/svn-community/svn@105501 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-x86_64/netsurf.sh
+++ repos/community-x86_64/netsurf.sh
@@ -3,5 +3,5 @@ shortname=`echo $LANG | cut -b1-2`
 if [[ -d /usr/share/netsurf/$shortname ]]; then
 	/usr/bin/netsurf.elf "$@"
 else
-	LANG=en_US /usr/bin/netsurf.elf "$@"
+	LANG=en /usr/bin/netsurf.elf "$@"
 fi
