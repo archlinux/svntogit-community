384aa18ef71c7a4b3aff8c88bd40061a9ea73370
svenstaro <svenstaro@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2014-03-30 20:36:14 +0000

    upgpkg: freecol 0.10.7-4
    Propely fix FS#39534
    
    git-svn-id: file:///srv/repos/svn-community/svn@108633 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/fc.sh
+++ trunk/fc.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 FC_PATH=/usr/share/java/freecol
-LOG_PATH=$(mktemp -d /tmp/freecol.XXX)
 
-[ ! -d "$LOG_PATH" ] && install -dm755 $LOG_PATH
-cd $LOG_PATH
+cd $(mktemp -d)
 java -Xmx512M -jar $FC_PATH/FreeCol.jar "$@" --freecol-data $FC_PATH/data --no-intro &> ./messages.log
