6f624a078569b7d33d1555669fff80b2fcc91292
alucryd <alucryd@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2014-09-15 15:52:42 +0000

    upgpkg: plex-home-theater 1.2.2-2
    git-svn-id: file:///srv/repos/svn-community/svn@119021 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- plex-home-theater/trunk/plexhometheater.sh
+++ plex-home-theater/trunk/plexhometheater.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-export XBMC_HOME=/opt/plexhometheater/share/XBMC
-exec /opt/plexhometheater/bin/plexhometheater
+export XBMC_BIN_HOME='/usr/lib/plexhometheater'
+export XBMC_HOME='/usr/share/plexhometheater'
+exec plexhometheater
