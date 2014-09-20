4926cf30966f719e311808b2b3f038fc2938bb87
muflone <muflone@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2014-09-20 17:38:24 +0000

    upgpkg: sweethome3d 4.4-3
    sweethome3d 4.4-3
    
    git-svn-id: file:///srv/repos/svn-community/svn@119330 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- sweethome3d/trunk/sweethome3d.sh
+++ sweethome3d/trunk/sweethome3d.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-/usr/bin/java -jar /usr/share/java/sweethome3d.jar
+JAVA_EXEC="java"
+if [ -n "$JAVA_HOME" ]; then
+  if [ -x "$JAVA_HOME/bin/java" ]; then
+    echo usa java home
+    JAVA_EXEC="$JAVA_HOME/bin/java"
+  fi
+else
+  echo usa which
+  JAVA_EXEC="$(which java)"
+fi
+
+if [ -z "$JAVA_EXEC" ]; then
+  echo "No 'java' executable can be found, please set JAVA_HOME variable or"
+  echo "use the 'archlinux-java' script to set the Java version."
+  exit 1
+fi
+
+"$JAVA_EXEC" -jar /usr/share/java/sweethome3d.jar
