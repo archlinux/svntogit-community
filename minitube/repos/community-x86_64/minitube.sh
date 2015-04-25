867c0a74572e3f7c479c85a0ca9108297c663c66
speps <speps@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2015-04-25 21:17:42 +0000

    archrelease: copy trunk to community-i686, community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@132248 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ minitube/repos/community-x86_64/minitube.sh
@@ -0,0 +1,9 @@
+# Google is now requiring an API key in order to access YouTube Data web services.
+# Create a "Browser Key" at https://console.developers.google.com and put it here.
+
+export GOOGLE_API_KEY=""
+
+# Uncomment the following line if you do use any
+# Desktop Environment and icons does not show up
+
+#export DESKTOP_SESSION="gnome"
