17609f62c3e3c7f4406d264182dc6dc3c00a37ce
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2015-09-18 22:26:31 +0000

    archrelease: copy trunk to community-any
    git-svn-id: file:///srv/repos/svn-community/svn@140663 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-any/grails.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-[[ -f $GRAILS_HOME/startGrails ]] || GRAILS_HOME=/usr/share/grails
-DIRNAME=$GRAILS_HOME
-. "$DIRNAME/startGrails"
-startGrails org.codehaus.groovy.grails.cli.GrailsScriptRunner "$@"
