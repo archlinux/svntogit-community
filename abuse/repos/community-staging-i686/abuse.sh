2b4f3fff4bb8ba5dafb0b8371c37dd9929bc2cf7
bluewind <bluewind@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2015-12-06 18:43:08 +0000

    archrelease: copy trunk to community-staging-x86_64, community-staging-i686
    git-svn-id: file:///srv/repos/svn-community/svn@149051 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ abuse/repos/community-staging-i686/abuse.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Use sane configuration files, unless they already exists
+
+if [ ! -d $HOME/.abuse ]; then
+  mkdir -p $HOME/.abuse
+fi
+if [ ! -f $HOME/.abuse/abuserc ]; then
+  cp /usr/share/abuse/default.abuserc $HOME/.abuse/abuserc
+fi
+if [ ! -f $HOME/.abuse/gamma.lsp ]; then
+  cp /usr/share/abuse/default.gamma.lsp $HOME/.abuse/gamma.lsp
+fi
+
+exec /usr/bin/abuse.elf
