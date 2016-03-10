179f4024cbc620e0419e78f50732c34e527b31bb
spupykin <spupykin@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2016-03-10 18:44:15 +0000

    upgpkg: st 0.6-3
    upd
    
    git-svn-id: file:///srv/repos/svn-community/svn@165986 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- st/trunk/config.h
+++ st/trunk/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Liberation Mono:pixelsize=12:antialias=false:autohint=false";
+static char font[] = "Fixed:pixelsize=18:antialias=false:autohint=false";
 static int borderpx = 2;
 static char shell[] = "/bin/sh";
 static char *utmp = NULL;
