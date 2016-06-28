40d1573cc56c889d029976f99716ea3682d00fb3
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2016-06-28 16:58:50 +0000

    Using the jEdit slackware package as basis instead of the jar installer
    git-svn-id: file:///srv/repos/svn-community/svn@181517 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- jedit/trunk/jedit.sh
+++ jedit/trunk/jedit.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+
 # Runs jEdit - Programmer's Text Editor
 
-# If $HOME/.jedit or $HOME/.jedit/properties exists, don't do anything.
-# If not, copy in a default property file.
+# If both $HOME/.jedit and $HOME/.jedit/properties doesn't exist,
+# copy in a default property file.
 if [ ! -e $HOME/.jedit ]; then
   if [ ! -e $HOME/.jedit/properties ]; then
-    mkdir $HOME/.jedit
-		cp /usr/share/jedit/default.props $HOME/.jedit/properties
+    mkdir -p "$HOME/.jedit"
+    cp "/usr/share/jedit/default.prop"s "$HOME/.jedit/properties"
   fi
 fi
 
-# Set jvm heap initial and maximum sizes (in megabytes).
+# Set the initial and maximum JVM heap size
 JAVA_HEAP_MAX_SIZE=192
 
+# Start jEdit
 exec java -Xmx${JAVA_HEAP_MAX_SIZE}M -Dawt.useSystemAAFontSettings=lcd -jar "/usr/share/java/jedit/jedit.jar" "$@"
