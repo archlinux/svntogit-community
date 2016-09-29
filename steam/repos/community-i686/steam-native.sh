44a86f66d77d3288d46e4a977254aa47c487ce0e
anthraxx <anthraxx@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2016-09-29 23:09:17 +0000

    archrelease: copy trunk to community-i686
    git-svn-id: file:///srv/repos/svn-community/svn@191033 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ steam/repos/community-i686/steam-native.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export STEAM_RUNTIME=0
+# Workaround for dbus fatal termination related coredumps (SIGABRT)
+# https://github.com/ValveSoftware/steam-for-linux/issues/4464
+export DBUS_FATAL_WARNINGS=0
+exec /usr/bin/steam $*
