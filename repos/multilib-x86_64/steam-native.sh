9c66939d1b5bc7649476de754d1c2323c5b1fa95
anthraxx <anthraxx@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2016-12-09 01:56:12 +0000

    archrelease: copy trunk to multilib-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@198499 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/multilib-x86_64/steam-native.sh
+++ repos/multilib-x86_64/steam-native.sh
@@ -3,4 +3,6 @@ export STEAM_RUNTIME=0
 # Workaround for dbus fatal termination related coredumps (SIGABRT)
 # https://github.com/ValveSoftware/steam-for-linux/issues/4464
 export DBUS_FATAL_WARNINGS=0
-exec /usr/bin/steam "$@"
+# Override some libraries as these are what games linked against.
+export LD_LIBRARY_PATH="/usr/lib/steam:/usr/lib32/steam"
+exec /usr/lib/steam/steam "$@"
