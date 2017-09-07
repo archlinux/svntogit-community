e2505dc048b92bc277bb8e6e7f25434a31461f1b
spupykin <spupykin@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2017-09-07 09:19:38 +0000

    archrelease: copy trunk to community-i686, community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@256099 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-x86_64/install.sh
+++ repos/community-x86_64/install.sh
@@ -1,7 +1,7 @@
 post_install() {
-    getent group  xmms2 > /dev/null || groupadd -r -g 46 xmms2
-    getent passwd xmms2 > /dev/null || useradd  -r -u 46 -g xmms2 -d /var/lib/xmms2 -s /bin/false -G audio xmms2
-    usr/bin/systemd-tmpfiles --create xmms2.conf
+    getent group  xmms2 > /dev/null || groupadd -r xmms2
+    getent passwd xmms2 > /dev/null || useradd  -r -g xmms2 -d /var/lib/xmms2 -s /bin/false -G audio xmms2
+    chown -R xmms2:xmms2 var/lib/xmms2
     true
 }
 
