8497231ab145f7e0209097d786dd049915b2c587
tensor5 <tensor5@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2017-10-14 10:43:08 +0000

    archrelease: copy trunk to community-i686, community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@262731 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- apm/repos/community-i686/apm.js
+++ apm/repos/community-i686/apm.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
-'use strict';
+"use strict";
 
 process.env.ATOM_RESOURCE_PATH = process.env.ATOM_RESOURCE_PATH ||
-        '/usr/lib/atom';
+        "/usr/lib/atom";
 
 process.env.ATOM_ELECTRON_VERSION = process.env.ATOM_ELECTRON_VERSION ||
-        require('fs')
-    .readFileSync('/usr/lib/electron/version', 'utf8').trim().slice(1);
+        require("fs")
+    .readFileSync("/usr/lib/electron/version", "utf8").trim().slice(1);
 
-process.env.PYTHON = __dirname + "/python-interceptor.sh";
+process.env.npm_config_python = __dirname + "/python-interceptor.sh";
 
-require('../lib/apm-cli.js').run(process.argv.slice(2), function (error) {
+require("../lib/apm-cli.js").run(process.argv.slice(2), function (error) {
     process.exitCode = +!!error;
 });
