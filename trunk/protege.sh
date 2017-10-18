3030046791d027a2fe08af1ad64de7e1963c1891
foxboron <foxboron@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2017-10-18 21:44:46 +0000

    Uploaded protege 5.2.0 from AUR
    git-svn-id: file:///srv/repos/svn-community/svn@263303 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ trunk/protege.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd /usr/share/java/protege
+
+CMD_OPTIONS="-Dapple.laf.useScreenMenuBar=true -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true" sh run.sh "$@"
