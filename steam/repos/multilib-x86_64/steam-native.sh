002e6c77f0d28988578a14177ec0a0cd28c2dfe1
anthraxx <anthraxx@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2017-12-05 21:47:06 +0000

    archrelease: copy trunk to multilib-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@272341 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- steam/repos/multilib-x86_64/steam-native.sh
+++ steam/repos/multilib-x86_64/steam-native.sh
@@ -4,5 +4,5 @@ export STEAM_RUNTIME=0
 # https://github.com/ValveSoftware/steam-for-linux/issues/4464
 export DBUS_FATAL_WARNINGS=0
 # Override some libraries as these are what games linked against.
-export LD_LIBRARY_PATH="/usr/lib/steam:/usr/lib32/steam:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib/steam:/usr/lib32/steam${LD_LIBRARY_PATH:+:}$LD_LIBRARY_PATH"
 exec /usr/lib/steam/steam "$@"
