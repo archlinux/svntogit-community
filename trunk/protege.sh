1462c8b48391fded70f6fe9df124d6f47fbd0c82
foxboron <foxboron@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-02-25 13:11:14 +0000

    upgpkg: protege 5.2.0-5
    Fixed #57629 by avoiding explicit shell call
    
    git-svn-id: file:///srv/repos/svn-community/svn@297950 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/protege.sh
+++ trunk/protege.sh
@@ -2,4 +2,4 @@
 
 cd /usr/share/java/protege
 
-CMD_OPTIONS="-Dapple.laf.useScreenMenuBar=true -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true" sh run.sh "$@"
+CMD_OPTIONS="-Dapple.laf.useScreenMenuBar=true -Dswing.defaultlaf=javax.swing.plaf.metal.MetalLookAndFeel -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dsun.java2d.xrender=true" ./run.sh "$@"
