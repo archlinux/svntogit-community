249e5cedb98ff97980901982a1c68cd801069658
foxboron <foxboron@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-07-05 17:55:47 +0000

    upgpkg: python-docs 3.6.6-1
    Updated to 3.6.6
    
    git-svn-id: file:///srv/repos/svn-community/svn@353029 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- python-docs/trunk/mirror-to-arch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-
-source PKGBUILD
-wget https://docs.python.org/3/archives/python-$pkgver-docs-html.tar.bz2
-scp python-$pkgver-docs-html.tar.bz2 repos.archlinux.org:/srv/sources/other/community/python-docs/
