9cf998703b17036704d26c77399d79a528aba9f6
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-08-13 12:33:37 +0000

    upgpkg: jedit 5.5.0-2
    git-svn-id: file:///srv/repos/svn-community/svn@371268 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- jedit/trunk/jedit.sh
+++ jedit/trunk/jedit.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-
-# Runs jEdit - Programmer's Text Editor
+#!/bin/sh
+# Run jEdit - Programmer's Text Editor
 
 # If both $HOME/.jedit and $HOME/.jedit/properties doesn't exist,
 # copy in a default property file.
 if [ ! -e $HOME/.jedit ]; then
   if [ ! -e $HOME/.jedit/properties ]; then
     mkdir -p "$HOME/.jedit"
-    cp "/usr/share/jedit/default.prop"s "$HOME/.jedit/properties"
+    cp /usr/share/jedit/default.props "$HOME/.jedit/properties"
   fi
 fi
 
@@ -15,4 +14,4 @@ fi
 JAVA_HEAP_MAX_SIZE=192
 
 # Start jEdit
-exec java -Xmx${JAVA_HEAP_MAX_SIZE}M -Dawt.useSystemAAFontSettings=lcd -jar "/usr/share/java/jedit/jedit.jar" "$@"
+exec java -Xmx${JAVA_HEAP_MAX_SIZE}M -Dawt.useSystemAAFontSettings=lcd -jar /usr/share/java/jedit/jedit.jar "$@"
