d50a39b42c95cbfedc04c3e203fab4aaacf2634d
bgyorgy <bgyorgy@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-09-13 15:14:56 +0000

    upgpkg: code 1.27.1-7
    Fix versioned source, remove unneeded variables from the launcher script
    
    git-svn-id: file:///srv/repos/svn-community/svn@380305 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/code.js
+++ trunk/code.js
@@ -1,32 +1,15 @@
 #!/usr/bin/electron
 
 const name = 'code'
-
 const app = require('electron').app;
 const path = require('path');
-const fs = require("fs");
-const Module = require("module");
-const {join} = require("path");
-const vm = require("vm");
-
-// Change command name.
-const fd = fs.openSync('/proc/self/comm', fs.constants.O_WRONLY);
-fs.writeSync(fd, name);
-fs.closeSync(fd);
 
 // Remove first command line argument (/usr/bin/electron).
 process.argv.splice(0, 1);
 
 // Set application paths.
 const appPath = path.join(path.dirname(__dirname), 'lib', name);
-const packageJson = require(path.join(appPath, 'package.json'));
-const productName = 'code-oss'
-app.setAppPath(appPath);
-app.setDesktopName(name + '.desktop');
-app.setName(productName);
-app.setPath('userCache', path.join(app.getPath('cache'), productName));
-app.setPath('userData', path.join(app.getPath('appData'), productName));
-app.setVersion(packageJson.version);
+app.setName(name);
 
 // Run the application.
 require('module')._load(appPath, module, true);
