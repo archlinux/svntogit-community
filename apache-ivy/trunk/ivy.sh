da5bd2d7460fc1305d67f6c3924a3d42bf0d5431
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-09-14 17:10:18 +0000

    Rename apache-ivy to just ivy
    git-svn-id: file:///srv/repos/svn-community/svn@380949 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- apache-ivy/trunk/ivy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ -z $CLASSPATH ]]; then
-  CLASSPATH="/usr/share/java/apache-ivy/ivy.jar"
-else
-  CLASSPATH="/usr/share/java/apache-ivy/ivy.jar:$CLASSPATH"
-fi
-exec "$JAVA_HOME/bin/java" -classpath $CLASSPATH $JAVA_OPTS org.apache.ivy.Main "$@"
