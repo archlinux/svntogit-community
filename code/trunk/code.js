c29df6bfbd56fc7ae5a3ccb2e42c86b45be81006
ffy00 <ffy00@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-09-14 00:46:03 +0000

    upgpkg: code 1.27.2-1
    git-svn-id: file:///srv/repos/svn-community/svn@380754 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- code/trunk/code.js
+++ code/trunk/code.js
@@ -1,15 +1,22 @@
 #!/usr/bin/electron
 
-const name = 'code'
+const name = 'code';
+
 const app = require('electron').app;
 const path = require('path');
 
-// Remove first command line argument (/usr/bin/electron).
+// Remove first command line argument (/usr/lib/code/code.js). - We call the CLI file first
 process.argv.splice(0, 1);
 
 // Set application paths.
-const appPath = path.join(path.dirname(__dirname), 'lib', name);
+const appPath = __dirname;
+const packageJson = require(path.join(appPath, 'package.json'));
+app.setAppPath(appPath);
+app.setDesktopName(name + '.desktop');
 app.setName(name);
+app.setPath('userCache', path.join(app.getPath('cache'), name));
+app.setPath('userData', path.join(app.getPath('appData'), name));
+app.setVersion(packageJson.version);
 
 // Run the application.
 require('module')._load(appPath, module, true);
