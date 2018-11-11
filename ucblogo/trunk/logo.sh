1016e9912b8d957e59e5d5d2d4857fd7d657556e
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2018-11-11 11:03:14 +0000

    upgpkg: ucblogo 6.0-8
    git-svn-id: file:///srv/repos/svn-community/svn@406348 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- ucblogo/trunk/logo.sh
+++ ucblogo/trunk/logo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export LOGOLIB=/usr/lib/logo/logolib
 if [[ -f "$1" ]]; then
-  cat "$1" | /usr/bin/logo.elf "$*" | head -n-2
+  /usr/bin/logo.elf "$*" < "$1" | head -n-2
 else
   /usr/bin/logo.elf "$@"
 fi
