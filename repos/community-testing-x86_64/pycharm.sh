97701669de89ef44b33d3ea67ec7519f3d0b21d5
anthraxx <anthraxx@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2019-07-24 22:17:24 +0000

    db-move: moved pycharm-community-edition from [community-testing] to [community] (x86_64)
    git-svn-id: file:///srv/repos/svn-community/svn@492727 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-testing-x86_64/pycharm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$PYCHARM_JDK" ] ; then
-  PYCHARM_JDK="/usr/lib/jvm/java-8-openjdk/"
-fi
-exec env PYCHARM_JDK=$PYCHARM_JDK /usr/share/pycharm/bin/pycharm.sh $@
-
-# vim: ts=2 sw=2 et:
-
