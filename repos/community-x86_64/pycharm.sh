6cb3e7a48beb6c177374366305a1d707ffa20aa8
alucryd <alucryd@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2019-09-09 13:44:18 +0000

    archrelease: copy trunk to community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@509097 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-x86_64/pycharm.sh
+++ repos/community-x86_64/pycharm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$PYCHARM_JDK" ] ; then
-  PYCHARM_JDK="/usr/lib/jvm/java-8-openjdk/"
+  PYCHARM_JDK="/usr/lib/jvm/java-11-openjdk/"
 fi
 exec env PYCHARM_JDK=$PYCHARM_JDK /usr/share/pycharm/bin/pycharm.sh $@
 
