f1694e08c5646482697b271e7d690252967401e8
anthraxx <anthraxx@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2019-09-26 08:41:36 +0000

    archrelease: copy trunk to community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@511815 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- pycharm-community-edition/repos/community-x86_64/pycharm.sh
+++ pycharm-community-edition/repos/community-x86_64/pycharm.sh
@@ -3,7 +3,7 @@
 if [ -z "$PYCHARM_JDK" ] ; then
   PYCHARM_JDK="/usr/lib/jvm/java-11-openjdk/"
 fi
-exec env PYCHARM_JDK=$PYCHARM_JDK /usr/share/pycharm/bin/pycharm.sh $@
+exec env PYCHARM_JDK="$PYCHARM_JDK" /usr/share/pycharm/bin/pycharm.sh "$@"
 
 # vim: ts=2 sw=2 et:
 
