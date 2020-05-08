5bf272c2504a0195175847f2c7ae8e68576d50e2
tensor5 <tensor5@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2020-05-08 01:17:56 +0000

    archrelease: copy trunk to community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@625510 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- apm/repos/community-x86_64/apm.js
+++ apm/repos/community-x86_64/apm.js
@@ -6,8 +6,6 @@ process.env.ATOM_RESOURCE_PATH = process.env.ATOM_RESOURCE_PATH || "/usr/lib/ato
 
 process.env.ATOM_ELECTRON_VERSION = process.env.ATOM_ELECTRON_VERSION || require("fs").readFileSync("/usr/lib/electron4/version", "utf8");
 
-process.env.npm_config_python = __dirname + "/python-interceptor.sh";
-
 require("../lib/apm-cli.js").run(process.argv.slice(2), function (error) {
     process.exitCode = Number(Boolean(error));
 });
