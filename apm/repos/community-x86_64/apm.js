eb708e998809734af3a579ad6dcbe3c69c7140d6
tensor5 <tensor5@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2020-05-23 04:06:29 +0000

    archrelease: copy trunk to community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@631829 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- apm/repos/community-x86_64/apm.js
+++ apm/repos/community-x86_64/apm.js
@@ -4,7 +4,7 @@
 
 process.env.ATOM_RESOURCE_PATH = process.env.ATOM_RESOURCE_PATH || "/usr/lib/atom";
 
-process.env.ATOM_ELECTRON_VERSION = process.env.ATOM_ELECTRON_VERSION || require("fs").readFileSync("/usr/lib/electron4/version", "utf8");
+process.env.ATOM_ELECTRON_VERSION = process.env.ATOM_ELECTRON_VERSION || require("fs").readFileSync("/usr/lib/electron5/version", "utf8");
 
 require("../lib/apm-cli.js").run(process.argv.slice(2), function (error) {
     process.exitCode = Number(Boolean(error));
