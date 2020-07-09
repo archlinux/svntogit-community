c3960af9dfa408d7dccac1331326e894abaa86df
foutrelis <foutrelis@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2020-07-09 17:54:04 +0000

    db-move: moved jsampler from [community-testing] to [community] (any)
    git-svn-id: file:///srv/repos/svn-community/svn@662268 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-testing-any/jsampler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export PATH="/usr/lib/jvm/java-8-openjdk/bin/:${PATH}"
-
-if [ $# -eq 0 ] || [ "$1" = "fantasia" ]; then
-  java -jar /usr/share/java/jsampler/fantasia.jar
-elif [ "$1" = "classic" ]; then
-  java -jar /usr/share/java/jsampler/classic.jar
-fi
