1811865d367bb59fe2deb105be9d04cb5ae0f99d
foutrelis <foutrelis@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2020-07-09 18:00:27 +0000

    db-move: moved rfc from [community-testing] to [community] (any)
    git-svn-id: file:///srv/repos/svn-community/svn@662733 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- repos/community-testing-any/mirror-to-arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -x
-
-source PKGBUILD
-wget https://www.rfc-editor.org/rfc/tar/RFC-all.tar.gz
-wget https://www.rfc-editor.org/rfc/rfc-index.txt
-scp RFC-all.tar.gz repos.archlinux.org:/srv/sources/other/community/rfc/RFC-all-$pkgver.tar.gz
-scp rfc-index.txt repos.archlinux.org:/srv/sources/other/community/rfc/rfc-index-$pkgver.txt
