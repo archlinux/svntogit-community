59d184afaeabd48870e2bcf8d77a429f977b1c04
anthraxx <anthraxx@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2020-07-19 22:52:02 +0000

    archrelease: copy trunk to community-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@664604 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- intellij-idea-community-edition/repos/community-x86_64/idea.sh
+++ intellij-idea-community-edition/repos/community-x86_64/idea.sh
@@ -6,6 +6,15 @@
 if [ -z "$IDEA_JDK" ] ; then
   IDEA_JDK="/usr/lib/jvm/java-11-openjdk/"
 fi
-exec env IDEA_JDK="$IDEA_JDK" /usr/share/idea/bin/idea.sh "$@"
+# open-jfx location that should match the JDK version
+if [ -z "$IDEA_JFX" ] ; then
+  IDEA_JFX="/usr/lib/jvm/java-11-openjfx/"
+fi
+# classpath according to defined JDK/JFX
+if [ -z "$IDEA_CLASSPATH" ] ; then
+  IDEA_CLASSPATH="${IDEA_JDK}/lib/*:${IDEA_JFX}/lib/*"
+fi
+
+exec env IDEA_JDK="$IDEA_JDK" IDEA_CLASSPATH="$IDEA_CLASSPATH" /usr/share/idea/bin/idea.sh "$@"
 
 # vim: ts=2 sw=2 et:
