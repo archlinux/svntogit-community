33f83a863cb96550e84b295accbbb150ed248483
arodseth <arodseth@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2020-12-22 16:38:49 +0000

    Set GDK_BACKEND=x11 when running Wings3D
    git-svn-id: file:///srv/repos/svn-community/svn@782655 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/wings.sh
+++ trunk/wings.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-exec /usr/bin/erl -noinput -smp \
+exec GDK_BACKEND=x11 /usr/bin/erl -noinput -smp \
   -pa /usr/lib/erlang/lib/esdl-*/ebin /usr/lib/wings3d/ebin \
   -run wings_start start_halt ${1+"$@"}
