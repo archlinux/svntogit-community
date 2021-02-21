045acaa3bd33823369d5430f72a9ae4a02f4c899
dvzrv <dvzrv@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2021-02-21 09:47:53 +0000

    upgpkg: nextcloud 21.0.0-6: Rebuild to make file installation more generic.
    Make the find call to install files of the web app more generic.
    Change the group ownership of directories below /usr/share/webapps/nextcloud
    back to root in the .install file (with nextcloud < 21.0.0 the files were
    recursively assigned to the http group, but this is not necessary).
    Install writable app and data dir with 770 file mode.
    Amend the .install message to not reference the previous (wrongly introduced)
    location of the configuration file.
    Amend the /usr/bin/occ command to allow overrides with NEXTCLOUD_USER,
    NEXTCLOUD_PHP and NEXTCLOUD_PHP_CONFIG (for user, php and php config
    respectively).
    
    git-svn-id: file:///srv/repos/svn-community/svn@867619 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- nextcloud/trunk/nextcloud.occ.sh
+++ nextcloud/trunk/nextcloud.occ.sh
@@ -2,9 +2,11 @@
 
 readonly default_config="/etc/php/php.ini"
 readonly default_php_command="/usr/bin/php"
+readonly default_user="nextcloud"
 
 config=""
 php_command=""
+user=""
 
 check_sudo() {
   if ! command -v sudo > /dev/null; then
@@ -16,17 +18,24 @@ check_sudo() {
 check_sudo
 
 # allow overriding the php.ini
-if [[ -n "${PHP_CONFIG}" ]] && [[ -f "${PHP_CONFIG}" ]]; then
-  config="${PHP_CONFIG}"
+if [[ -n "${NEXTCLOUD_PHP_CONFIG}" ]] && [[ -f "${NEXTCLOUD_PHP_CONFIG}" ]]; then
+  config="${NEXTCLOUD_PHP_CONFIG}"
 else
   config="${default_config}"
 fi
 
 # allow overriding the php executable
-if [[ -n "${PHP}" ]] && command -v "${PHP}" > /dev/null; then
-  php_command="${PHP}"
+if [[ -n "${NEXTCLOUD_PHP}" ]] && command -v "${NEXTCLOUD_PHP}" > /dev/null; then
+  php_command="${NEXTCLOUD_PHP}"
 else
   php_command="${default_php_command}"
 fi
 
-sudo -u nextcloud "$php_command" -c "${config}" /usr/share/webapps/nextcloud/occ "$@"
+# allow overriding the user to run as
+if [[ -n "${NEXTCLOUD_USER}" ]] && uid "${NEXTCLOUD_USER}" > /dev/null 2>&1; then
+  user="${NEXTCLOUD_USER}"
+else
+  user="${default_user}"
+fi
+
+sudo -u "$user" "$php_command" -c "${config}" /usr/share/webapps/nextcloud/occ "$@"
