4fb74b9b60272e0724712ad9ae0c729f3bb188ce
tensor5 <tensor5@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2021-06-18 02:58:35 +0000

    db-move: moved code from [community-staging] to [community] (x86_64)
    git-svn-id: file:///srv/repos/svn-community/svn@965095 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- code/repos/community-staging-x86_64/code.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# don't edit the electron binary name here! simply change the variable in the PKGBUILD and we will sed it into the correct one :)
-ELECTRON_RUN_AS_NODE=1 exec electron12 /usr/lib/code/out/cli.js /usr/lib/code/code.js "$@"
