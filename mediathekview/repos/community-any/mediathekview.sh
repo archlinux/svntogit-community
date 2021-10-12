63088d964ea92a8e6a3e5a4f42b4250e5f160050
dvzrv <dvzrv@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2021-10-12 15:42:58 +0000

    archrelease: copy trunk to community-any
    git-svn-id: file:///srv/repos/svn-community/svn@1029902 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- mediathekview/repos/community-any/mediathekview.sh
+++ mediathekview/repos/community-any/mediathekview.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-PATH="/usr/lib/jvm/java-16-openjdk/bin:${PATH}"
+PATH="/usr/lib/jvm/java-17-openjdk/bin:${PATH}"
 # requires preview features: https://github.com/mediathekview/MediathekView/issues/528
 java -jar /usr/share/java/mediathekview/MediathekView.jar "$@"
