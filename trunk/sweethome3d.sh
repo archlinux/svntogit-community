b7055d653d2d4d0822d487dcb1c4391eba102702
muflone <muflone@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2021-11-28 23:40:29 +0000

    upgpkg: sweethome3d 6.6-3
    git-svn-id: file:///srv/repos/svn-community/svn@1055866 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/sweethome3d.sh
+++ trunk/sweethome3d.sh
@@ -24,6 +24,25 @@ fi
 # Find Java version
 JAVA_VERSION="$(${JAVA_EXEC} -version 2>&1 | head -1 | cut -d' ' -f 3 | tr -d '"')"
 
+# Skip Java versions higher than 16 as Sweethome3D actually is not yet compatible
+# Fallback to any previous version available
+# https://bugs.archlinux.org/task/72623
+if [ $(vercmp "${JAVA_VERSION}" "17") -gt 0 ]
+then
+  echo "Warning: Sweethome 3D actually is not compatible with Java version > 16"
+  _PREVIOUS_JAVA_VERSION="$(archlinux-java status | tail -n +2 | sort | cut -d ' ' -f 3 | sort -nr -k 2 -t '-' | grep -vE '17-' -m 1)"
+  if [ -z "${_PREVIOUS_JAVA_VERSION}" ]
+  then
+    echo "No others Java version are available, please install a Java version < 17"
+    exit 1
+  else
+    echo "Falling back to ${_PREVIOUS_JAVA_VERSION}"
+    export JAVA_HOME="/usr/lib/jvm/${_PREVIOUS_JAVA_VERSION}"
+    export JAVA_EXEC="${JAVA_HOME}/bin/java"
+    export JAVA_VERSION="$(${JAVA_EXEC} -version 2>&1 | head -1 | cut -d' ' -f 3 | tr -d '"')"
+  fi
+fi
+
 # Fix for Java 16 compatibility
 # https://bugs.archlinux.org/task/71255
 # https://sourceforge.net/p/sweethome3d/bugs/1021/
