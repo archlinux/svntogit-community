1def5ea890fbc57e95dba6ffcfa334b3c2e04e89
mtorromeo <mtorromeo@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2022-09-06 16:19:11 +0000

    upgpkg: code 1.71.0-2: fixed loading of electron-flags.conf and code-flags.conf
    git-svn-id: file:///srv/repos/svn-community/svn@1294454 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- code/trunk/code.sh
+++ code/trunk/code.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+set -euo pipefail
+
+name=code
+flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/${name}-flags.conf"
+
+declare -a flags
+
+if [[ -f "${flags_file}" ]]; then
+    mapfile -t < "${flags_file}"
+fi
+
+for line in "${MAPFILE[@]}"; do
+    if [[ ! "${line}" =~ ^[[:space:]]*#.* ]]; then
+        flags+=("${line}")
+    fi
+done
+
 # don't edit the electron binary name here! simply change the variable in the PKGBUILD and we will sed it into the correct one :)
-ELECTRON_RUN_AS_NODE=1 exec electron /usr/lib/code/out/cli.js /usr/lib/code/code.js "$@"
+exec electron /usr/lib/code/out/cli.js /usr/lib/code/code.js "${flags[@]}" "$@"
