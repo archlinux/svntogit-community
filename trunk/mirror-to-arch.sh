59ecf6174247500dc289a63671741e4de0d5bab8
spupykin <spupykin@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2022-10-12 21:32:14 +0000

    upgpkg: rfc 20221012-1
    git-svn-id: file:///srv/repos/svn-community/svn@1327928 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- trunk/mirror-to-arch.sh
+++ trunk/mirror-to-arch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash -x
 
 source PKGBUILD
-wget https://www.rfc-editor.org/rfc/tar/RFC-all.tar.gz
-wget https://www.rfc-editor.org/rfc/rfc-index.txt
-scp RFC-all.tar.gz repos.archlinux.org:/srv/sources/other/community/rfc/RFC-all-$pkgver.tar.gz
-scp rfc-index.txt repos.archlinux.org:/srv/sources/other/community/rfc/rfc-index-$pkgver.txt
+test -f RFC-all.tar.gz || wget https://www.rfc-editor.org/rfc/tar/RFC-all.tar.gz
+test -f rfc-index.txt || wget https://www.rfc-editor.org/rfc/rfc-index.txt
+
+#scp RFC-all.tar.gz repos.archlinux.org:/srv/sources/other/community/rfc/RFC-all-$pkgver.tar.gz
+#scp rfc-index.txt repos.archlinux.org:/srv/sources/other/community/rfc/rfc-index-$pkgver.txt
+
+scp RFC-all.tar.gz p5n.pp.ru:/home/sergej/public_html/dl/RFC-all-$pkgver.tar.gz
+scp rfc-index.txt p5n.pp.ru:/home/sergej/public_html/dl/rfc-index-$pkgver.txt
