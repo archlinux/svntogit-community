c3b11f2e640b504a48c5ac1e4eb41f339713a4d1
felixonmars <felixonmars@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2022-11-01 22:22:43 +0000

    archrelease: copy trunk to community-staging-x86_64
    git-svn-id: file:///srv/repos/svn-community/svn@1340405 9fca08f4-af9d-4005-b8df-a31f2cc04f65

---
+++ ejabberd/repos/community-staging-x86_64/print-deps.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Helper script which prints ejabberd deps' urls including commit hash
+#
+
+test -d src/ejabberd-*/config || makepkg -od || exit 1
+
+cd src/ejabberd-*/config && cd ..
+
+rm -rf deps
+
+[ -x configure ] || ./autogen.sh
+./configure --enable-all
+make deps || exit 1
+
+cd deps ; ls -1 | while read g; do
+	test -d $g && \
+	pushd $g &>/dev/null
+	if [ $? -eq 0 ]; then
+		U=`grep 'url =' .git/config | cut -f2 -d= | tr -d ' '`
+		C=`git rev-parse HEAD`
+		N=`basename $(pwd)`
+		echo -e "\t\"$N::$U#commit=$C\""
+	fi
+	popd &>/dev/null
+done | sed 's|https://|git+https://|' >../../../PKGBUILD.inc
+
+cd ../../..
+cat PKGBUILD.inc | cut -f2 -d'"' | cut -f1 -d: | tr '\n' ' ' >PKGBUILD.inc2
