c8bf3ae2e3deab793cb8e8544250ef943d14c85e
mtorromeo <mtorromeo@9fca08f4-af9d-4005-b8df-a31f2cc04f65>
2023-03-14 21:25:31 +0000

    upgpkg: code 1.76.1-2: Changed bin script to understand electron flags in combination with ELECTRON_RUN_AS_NODE (Fixes FS#75667)
    git-svn-id: file:///srv/repos/svn-community/svn@1420495 9fca08f4-af9d-4005-b8df-a31f2cc04f65

--- code/trunk/code.sh
+++ code/trunk/code.sh
@@ -2,10 +2,9 @@
 
 set -euo pipefail
 
-name=code
-flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/${name}-flags.conf"
+flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/code-flags.conf"
 
-declare -a flags
+declare -a codeflags
 
 if [[ -f "${flags_file}" ]]; then
     mapfile -t < "${flags_file}"
@@ -13,9 +12,24 @@ fi
 
 for line in "${MAPFILE[@]}"; do
     if [[ ! "${line}" =~ ^[[:space:]]*#.* ]]; then
-        flags+=("${line}")
+        codeflags+=("${line}")
     fi
 done
 
 # don't edit the electron binary name here! simply change the variable in the PKGBUILD and we will sed it into the correct one :)
-exec electron /usr/lib/code/out/cli.js /usr/lib/code/code.js "${flags[@]}" "$@"
+name=electron
+flags_file="${XDG_CONFIG_HOME:-$HOME/.config}/${name}-flags.conf"
+
+declare -a electronflags
+
+if [[ -f "${flags_file}" ]]; then
+    mapfile -t < "${flags_file}"
+fi
+
+for line in "${MAPFILE[@]}"; do
+    if [[ ! "${line}" =~ ^[[:space:]]*#.* ]]; then
+        electronflags+=("${line}")
+    fi
+done
+
+ELECTRON_RUN_AS_NODE=1 exec /usr/lib/${name}/electron /usr/lib/code/out/cli.js /usr/lib/code/code.js "${electronflags[@]}" "${codeflags[@]}" "$@"
